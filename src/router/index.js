15a0cfd8756511976341750b2a80db2f11e599f7
fpestunov <fpestunov@gmail.com>
2020-03-06 13:10:08 +0400

    add 01 layouts and app settings

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,18 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
   }
 ];
 
